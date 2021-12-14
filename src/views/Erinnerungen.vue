731153cbd314a2c80b1c0523d7a65073b77db25e
sinan <s0576403@htw-berlin.de>
2021-12-14 19:24:58 +0100

    Erinnerungen erneuert

--- src/views/Erinnerungen.vue
+++ src/views/Erinnerungen.vue
@@ -1,34 +1,52 @@
 <template>
   <h1>Hier sind deine Erinnerungen</h1>
-  <div class="card-group">
-    <div class="card">
-      <img src="../assets/Ha.png" class="card-img-top" alt="">
-      <div class="card-body">
-        <h5 class="card-title">Erste Erinnerung</h5>
-        <p class="card-text">Vergiss nicht deine Hausaufgaben zu erledigen</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 45 mins ago</small>
-      </div>
-    </div>
-    <div class="card">
-      <img src="../assets/kochen.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Zweite Erinnerung</h5>
-        <p class="card-text">Vergiss nicht zu kochen</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 6h ago</small>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/planen2.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Erste Erinnerung</h5>
+          <p class="card-text">Vergiss nicht den morgigen Tag zu planen</p>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="card">
-      <img src="../assets/Sport.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Dritte Erinnerung</h5>
-        <p class="card-text">Vergiss nicht Sport zu machen</p>
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/Ha.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Zweite Erinnerung</h5>
+          <p class="card-text">Vergiss nicht deine Hausaufgaben zu erledigen</p>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
       </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 23h ago</small>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/kochen.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Dritte Erinnerung</h5>
+          <p class="card-text">Vergiss nicht zu kochen</p>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/Sport.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Vierte Erinnerung</h5>
+          <p class="card-text">Vergiss nicht Sport zu machen</p>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -38,8 +56,6 @@
 export default {
   name: 'Erinnerungen'
 }
-
 </script>
-
 <style scoped>
 </style>
