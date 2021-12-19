50385328349f42f3b653f3436cb13d3089f3816d
sinan <s0576403@htw-berlin.de>
2021-12-19 01:52:40 +0100

    Endpoints

--- src/views/Erinnerungen.vue
+++ src/views/Erinnerungen.vue
@@ -3,14 +3,14 @@
   <div class="row row-cols-1 row-cols-md-3 g-4">
     <div class="col" v-for="erinnerung in erinnerungen" :key="erinnerung.id">
       <div class="card h-100">
-        <img :src="getAvatar(erinnerung)" class="card-img-top" alt=" erinnerung.ersteAufgabe + ' ' + erinnerung.nachsteAufgabe">
+        <img :src="getAvatar(erinnerung)" class="card-img-top" alt="erinnerung.ersteAufgabe + ' ' + erinnerung.nachsteAufgabe">
         <div class="card-body">
           <h5 class="card-title">{{ erinnerung.ersteAufgabe }} {{ erinnerung.nachsteAufgabe}} </h5>
           <p class="card-text">
             {{ erinnerung.ersteAufgabe }} {{ erinnerung.nachsteAufgabe}} ist {{ erinnerung.erledigt ? 'erledigt' : 'nicht erledigt'}}
           </p>
           <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+            <small class="text-muted">Last updated 1 sec ago</small>
           </div>
         </div>
       </div>
@@ -23,28 +23,13 @@ export default {
   name: 'Erinnerungen',
   data () {
     return {
-      erinnerungen: [
-        {
-          id: 1,
-          ersteAufgabe: 'sport',
-          nachsteAufgabe: 'kochen',
-          erledigt: true,
-          job: 'sport'
-        },
-        {
-          id: 2,
-          ersteAufgabe: 'kochen',
-          nachsteAufgabe: 'sport',
-          erledigt: true,
-          job: 'kochen'
-        }
-      ]
+      erinnerungen: []
     }
   },
   methods: {
     getAvatar (erinnerung) {
       if (erinnerung.job === 'planen') {
-        return require('../assets/planen2.jpeg')
+        return require('../assets/planen.png')
       } else if (erinnerung.job === 'sport') {
         return require('../assets/Sport.png')
       } else if (erinnerung.job === 'kochen') {
@@ -57,6 +42,19 @@ export default {
         return require('../assets/Ha.png')
       }
     }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/erinnerung'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(erinnerung => {
+        this.erinnerungen.push(erinnerung)
+      }))
+      .catch(error => console.log('error', error))
   }
 }
 </script>
