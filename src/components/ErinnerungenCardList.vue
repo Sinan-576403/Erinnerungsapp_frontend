10ebd8d6e67f6c97627a4955836feaf03ba5bb7e
sinan <s0576403@htw-berlin.de>
2022-07-04 22:56:10 +0200

    ErinnerungenCardList.vue Erneuert

--- src/components/ErinnerungenCardList.vue
+++ src/components/ErinnerungenCardList.vue
@@ -1,14 +1,17 @@
 <template>
-  <h1>Hier sind deine Erinnerungen</h1>
+  <h1 style="color:red;">Deine Erinnerungen</h1>
   <div class="row row-cols-1 row-cols-md-3 g-5">
     <div class="col" v-for="erinnerung in erinnerungen" :key="erinnerung.id">
       <div class="card h-100">
         <img :src="getAvatar(erinnerung)" class="card-img-top" alt="erinnerung.ersteAufgabe + ' ' + erinnerung.nachsteAufgabe">
         <div class="card-body">
-          <h5 class="card-title">{{ erinnerung.ersteAufgabe }} {{ erinnerung.nachsteAufgabe}} </h5>
+          <h5 class="card-title">{{erinnerung.id}}. {{ erinnerung.ersteAufgabe }} Art:{{ erinnerung.art }} </h5>
           <p class="card-text">
             {{ erinnerung.ersteAufgabe }} ist {{ erinnerung.erledigt ? 'erledigt' : 'nicht erledigt'}} + {{ erinnerung.nachsteAufgabe}}
             muss noch {{ erinnerung.erledigt ? 'erledigt' : 'nicht erledigt'}} werden
+          </p>
+          <p class="card-text">
+            {{ erinnerung.ersteAufgabe }} ist {{ erinnerung.erledigt ? 'erledigt' : 'nicht erledigt'}}
           </p>
         </div>
       </div>
